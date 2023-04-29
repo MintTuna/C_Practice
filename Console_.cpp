30a210182d64f14faaaac338b221188e05ddbbc6
MintTuna <100212981+MintTuna@users.noreply.github.com>
2023-04-30 08:01:27 +0900

    Add files via upload

---
+++ Console_.cpp
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<windows.h>
+#include <conio.h>
+
+//key define
+#define UP 0
+#define DOWN 1
+#define LEFT 2
+#define RIGHT 3
+#define ESC 4
+#define SPACE 5
+ 
+int keyControl();
+void gotoxy(int, int);
+
+int main() {
+	system("mode con cols=60 lines=29");
+	int xpos = 0;
+	int ypos = 0;
+	while (true) {
+		int keyCode = keyControl();
+
+		if (keyCode == 0) {
+			ypos = ypos - 1;
+			//system("cls");
+			gotoxy(xpos, ypos);
+			//printf("бс");
+		}
+		else if (keyCode == 1) {
+			ypos = ypos + 1;
+			//system("cls");
+			gotoxy(xpos, ypos);
+			//printf("бс");
+		}
+		else if (keyCode == 2) {
+			xpos = xpos - 2;
+			//system("cls");
+			gotoxy(xpos, ypos);
+			//printf("бс");
+		}
+		else if (keyCode == 3) {
+			xpos = xpos + 2;
+			//system("cls");
+			gotoxy(xpos, ypos);
+			//printf("бс");
+		}
+		else if (keyCode == 4) {
+			system("cls");
+			xpos = 0;
+			ypos = 0;
+		}
+		else if (keyCode == 5) {
+			printf("бс");
+			//xpos = 0;
+			//ypos = 0;
+		}
+
+	}
+
+
+	return 0;
+}
+
+int keyControl() {
+	char temp = _getch();
+
+	if (temp == 'w' || temp == 'W') {
+		return UP;
+	}
+	else if (temp == 'a' || temp == 'A') {
+		return LEFT;
+	}
+	else if (temp == 's' || temp == 'S') {
+		return DOWN;
+	}
+	else if (temp == 'd' || temp == 'D') {
+		return RIGHT;
+	}
+	else if (temp == 27 || temp == 'ESC') {
+		return ESC;
+	}
+	else if (temp == ' ') {
+		return SPACE;
+	}
+}
+
+void gotoxy(int x, int y) {
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD pos;
+	pos.X = x;
+	pos.Y = y;
+	SetConsoleCursorPosition(consoleHandle, pos);
+}
+
+ 
